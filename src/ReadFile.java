e4b072d83fbfa2e035b22b28b74be988d1f22a07
cs.elys <cs.elys@gmail.com>
2013-05-28 22:54:37 -0400

    new add

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,3 +1,6 @@
+import java.io.*;
+
+
 /**
  * Created with IntelliJ IDEA.
  * User: xps
@@ -6,4 +9,67 @@
  * To change this template use File | Settings | File Templates.
  */
 public class ReadFile {
+    String strFileName;
+
+    public ReadFile (String strName){
+        strFileName=strName;
+    }
+
+    public BufferedReader GetBuffer(){
+        BufferedReader br = null;
+
+        try {
+            System.out.println("Enter something here : ");
+
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            String s = bufferRead.readLine();
+
+            br = new BufferedReader(new FileReader(s+".txt"));
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return br;
+    }
+
+
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+
+        try {
+            System.out.println("Enter something here : ");
+
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            String s = bufferRead.readLine();
+
+            System.out.println(s);
+
+
+            String sCurrentLine;
+
+
+            br = new BufferedReader(new FileReader(s+".txt"));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
 }
